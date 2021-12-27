27cecfc36a8c0834ac7dec6acff4650fd5de7d9c
ivan3scence <ivan.gorich@gmail.com>
2021-12-28 01:02:19 +0300

    asf

--- cheeeel/zakup2_rabochiy.py
+++ cheeeel/zakup2_rabochiy.py
@@ -13,7 +13,7 @@ record_file = 'z2_new.csv'  # файл с результатами
 pages = 1
 
 def urrll(i):
-    url = 'https://zakupki.gov.ru/epz/dizk/search/results.html?morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&sortBy=UPDATE_DATE&pageNumber=1&sortDirection=false&recordsPerPage=_100&showLotsInfoHidden=false&published=on&ur=on&customerPlace=5277335%2C5277327&customerPlaceCodes=%2C&updateDateFrom=22.12.2021&updateDateTo=23.12.2021'
+    url = 'https://zakupki.gov.ru/epz/dizk/search/results.html?morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&sortBy=UPDATE_DATE&pageNumber=1&sortDirection=false&recordsPerPage=_100&showLotsInfoHidden=false&published=on&ur=on&customerPlace=5277335%2C5277327&customerPlaceCodes=%2C&updateDateFrom=27.12.2021&updateDateTo=28.12.2021'
     return url
 
 def request_url(url):
